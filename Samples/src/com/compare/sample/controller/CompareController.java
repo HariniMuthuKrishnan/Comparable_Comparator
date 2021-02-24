a70c761bcbd43b1071a945ec749b00f1edf164dd
harini.muthukrishnan <harini.muthukrishnan@INDIA-HariniMK.perficient.com>
2021-02-24 17:52:04 +0530

    Hashcode and equals method overriding

--- Samples/src/com/compare/sample/controller/CompareController.java
+++ Samples/src/com/compare/sample/controller/CompareController.java
@@ -1,8 +1,14 @@
 package com.compare.sample.controller;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-import com.compare.sample.model.*;
+import com.compare.sample.model.Laptop;
+import com.compare.sample.model.Student;
 
 public class CompareController {
 
@@ -16,12 +22,17 @@ public class CompareController {
 		Collections.sort(studs);
 		for (Student std : studs)
 			System.out.println(std);
+
 		// Comparator
 		List<Laptop> laps = new ArrayList<>();
-		laps.add(new Laptop(5, "Dell", 500));
-		laps.add(new Laptop(8, "Hp", 900));
-		laps.add(new Laptop(3, "Asus", 480));
-		laps.add(new Laptop(6, "Toshiba", 209));
+		Laptop lap1 = new Laptop(5, "Dell", 500);
+		Laptop lap2 = new Laptop(5, "Dell", 560);
+		Laptop lap3 = new Laptop(3, "Asus", 480);
+		Laptop lap4 = new Laptop(6, "Toshiba", 209);
+		laps.add(lap1);
+		laps.add(lap2);
+		laps.add(lap3);
+		laps.add(lap4);
 		Comparator<Laptop> com = new Comparator<Laptop>() {
 			@Override
 			public int compare(Laptop lap1, Laptop lap2) {
@@ -38,5 +49,13 @@ public class CompareController {
 		for (Laptop lap : laps)
 			System.out.println(lap);
 
+		// Hashcode and equals method overriding
+		Set lapSet = new HashSet<>();
+		lapSet.add(lap1);
+		lapSet.add(lap2);
+		lapSet.add(lap3);
+		lapSet.add(lap4);
+		System.out.println("Set Values are");
+		System.out.println(lapSet);
 	}
 }
--- Samples/src/com/compare/sample/model/Laptop.java
+++ Samples/src/com/compare/sample/model/Laptop.java
@@ -40,4 +40,34 @@ public class Laptop {
 	public String toString() {
 		return "Laptop [lapid=" + lapid + ", lapname=" + lapname + ", price=" + price + "]";
 	}
+
+	/*
+	 * Comparing all the attributes
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((lapid == null) ? 0 : lapid.hashCode());
+		result = prime * result + ((lapname == null) ? 0 : lapname.hashCode());// Can be neglected when no need to
+																				// consider name
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Laptop other = (Laptop) obj;
+		if (lapid == null) {
+			if (other.lapid != null)
+				return false;
+		} else if (!lapid.equals(other.lapid))
+			return false;
+		//// Can be neglected when no need to consider name
+		if (lapname == null) {
+			if (other.lapname != null)
+				return false;
+		} else if (!lapname.equals(other.lapname))
+			return false;
+		return true;
+	}
 }
